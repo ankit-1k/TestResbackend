20c3b977f74b18087ab7b8f9715ef77b795e2cc9
ankit-1k <ankitpanda922@gmail.com>
2024-10-12 14:58:16 +0530

    u1 login data pass to booking backend removed b...

--- srcnode/router/routerAuth.js
+++ srcnode/router/routerAuth.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const routerAuth = express.Router();
 const User = require("../schema/User");
-const bcrypt = require("bcryptjs"); 
+// const bcrypt = require("bcryptjs"); 
 
 routerAuth.post("/register", async (req, res) => {
-  const { username, email, password } = req.body;
+  const { username, email,phone, password } = req.body;
   try {
     const userExists = await User.findOne({ email });
     if (userExists) {
       return res.status(400).send("User already exists");
     }
 
-    const newUser = new User({ username, email, password });
+    const newUser = new User({ username, email,phone, password });
     await newUser.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -19,32 +19,22 @@ routerAuth.post("/register", async (req, res) => {
   }
 });
 
-routerAuth.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    console.log("Received login request:", { email, password });
+routerAuth.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (!user || user.password !== password) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
   
-    try {
-      const user = await User.findOne({ email });
-      if (!user) {
-        console.log("User not found");
-        return res.status(400).send("Invalid email or password");
-      }
-  
-      // Compare provided password with stored hashed password
-      const isMatch = await bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        console.log("Password mismatch");
-        return res.status(400).send("Invalid email or password");
-      }
-  
-      res.status(200).json({ message: "Login successful" });
-    } catch (error) {
-      console.log("Server error:", error);
-      res.status(500).send("Server error");
+  res.status(200).json({ 
+    message: 'Login successful',
+    user: {
+      username: user.username,
+      email: user.email,
+      phone: user.phone
     }
   });
-  
-
-    
+});
 
 module.exports = routerAuth;
