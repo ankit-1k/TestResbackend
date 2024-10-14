ddafeb725fe62d82fdaab08e8019c682843110d6
ankit-1k <ankitpanda922@gmail.com>
2024-10-14 23:20:06 +0530

    order delete success

--- srcnode/router/orderRouter.js
+++ srcnode/router/orderRouter.js
@@ -26,5 +26,34 @@ orderRouter.get('/orders', async (req, res) => {
     res.status(500).json({ error: 'Failed to retrieve orders' });
   }
 });
+orderRouter.delete('/orders/:id', async (req, res) => {
+  try {
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (!order) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    res.json({ message: 'Order deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+orderRouter.put('/orders/:id', async (req, res) => {
+  const { name, table, items, total } = req.body;
 
+  try {
+    const updatedOrder = await Order.findByIdAndUpdate(
+      req.params.id,
+      { name, table, items, total },
+      { new: true, runValidators: true } // new: true returns the updated document
+    );
+
+    if (!updatedOrder) {
+      return res.status(404).json({ message: 'Order not found' });
+    }
+    
+    res.json(updatedOrder);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 module.exports = orderRouter;
