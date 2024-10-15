14ab1e04f365a95e30f7c1361014885eee200a95
ankit-1k <ankitpanda922@gmail.com>
2024-10-15 21:58:43 +0530

    menu working adding modal for update

--- srcnode/adminRouter/adMenu.js
+++ srcnode/adminRouter/adMenu.js
@@ -7,19 +7,18 @@ router.post("/postmenu", async (req, res) => {
   try {
     const { category, items } = req.body;
 
-    // Find the existing category
     let menuItem = await MenuItem.findOne({ category });
 
     if (menuItem) {
-      // If the category exists, push the new item to the items array
       menuItem.items.push(...items);
     } else {
-      // If the category doesn't exist, create a new menu item
       menuItem = new MenuItem({ category, items });
     }
 
     await menuItem.save();
-    res.status(201).json({ message: "Menu item added successfully!", menuItem });
+    res
+      .status(201)
+      .json({ message: "Menu item added successfully!", menuItem });
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -29,6 +28,43 @@ router.get("/getmenu", async (req, res) => {
   try {
     const menuItems = await MenuItem.find();
     res.status(200).json(menuItems);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// DELETE /api/deletemenu/:id
+router.delete("/deletemenu/:id/:itemIndex", async (req, res) => {
+  try {
+    const { id, itemIndex } = req.params;
+
+    const menuItem = await MenuItem.findById(id);
+    if (menuItem) {
+      menuItem.items.splice(itemIndex, 1); // Remove item by index
+      await menuItem.save();
+      return res
+        .status(200)
+        .json({ message: "Menu item deleted successfully!" });
+    }
+    res.status(404).json({ message: "Menu item not found" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// PUT /api/updatemenu/:id/:itemIndex
+router.put("/updatemenu/:id/:itemIndex", async (req, res) => {
+  try {
+    const { id, itemIndex } = req.params;
+    const { img, name, message, price } = req.body;
+
+    const menuItem = await MenuItem.findById(id);
+    if (menuItem) {
+      menuItem.items[itemIndex] = { img, name, message, price }; // Update the specific item
+      await menuItem.save();
+      return res
+        .status(200)
+        .json({ message: "Menu item updated successfully!", menuItem });
+    }
+    res.status(404).json({ message: "Menu item not found" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
